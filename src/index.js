e94399424bbca20e0411f9afde33361fb4ee0341
Filip Strandberg <filip.strandberg@hyph.com>
2023-08-06 16:59:47 +0200

    hexagon start

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.section-container');
 
+// TODO: Can't zoom in
 container.addEventListener('wheel', (event) => {
   event.preventDefault();
   const delta = event.deltaY;
