e9cfe9913b25a9d7d3573523a89f5c390ba63503
Filip Strandberg <filip.strandberg@hyph.com>
2023-08-20 15:53:20 +0200

    WIP diffirent corner borders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const container = document.querySelector('.section-container');
 
 // TODO: Can't zoom in
-container.addEventListener('wheel', (event) => {
-  event.preventDefault();
-  const delta = event.deltaY;
+// container.addEventListener('wheel', (event) => {
+//   event.preventDefault();
+//   const delta = event.deltaY;
 
-  container.scrollBy({ top: delta, behavior: 'smooth' });
-});
+//   container.scrollBy({ top: delta, behavior: 'smooth' });
+// });
+
+// https://stackoverflow.com/a/66029649/11763719
+function scrollHandler(e) {
+  var atSnappingPoint = e.target.scrollTop % e.target.offsetHeight === 0;
+  var timeOut = atSnappingPoint ? 0 : 150; //see notes
+
+  clearTimeout(e.target.scrollTimeout); //clear previous timeout
+
+  e.target.scrollTimeout = setTimeout(function () {
+    console.log('Scrolling stopped!');
+  }, timeOut);
+}
+
+container.addEventListener('scroll', scrollHandler);
