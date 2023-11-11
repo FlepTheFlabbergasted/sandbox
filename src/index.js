9f9283501961787a759f29ba287c762a56abe147
Filip Strandberg <filip.strandberg@hyph.com>
2023-11-11 16:01:35 +0100

    wip, initial scroll down works

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
-// https://stackoverflow.com/a/66029649/11763719
-function scrollHandler(e) {
-  var atSnappingPoint = e.target.scrollTop % e.target.offsetHeight === 0;
-  var timeOut = atSnappingPoint ? 0 : 150; //see notes
+const body = document.body;
+const container = document.querySelector('#section-container');
+const sections = document.querySelectorAll('.section');
 
-  clearTimeout(e.target.scrollTimeout); //clear previous timeout
+body.onload = () => {
+  if (window.location.hash) {
+    scrollToSection(window.location.hash);
+  } else {
+    setSectionUrlHash(sections[0].id);
+  }
 
-  e.target.scrollTimeout = setTimeout(function () {}, timeOut);
+  container.addEventListener('scrollend', scrollEndHandler);
+};
+
+function scrollEndHandler() {
+  for (const section of sections) {
+    const rect = section.getBoundingClientRect();
+    const isVisible = rect.bottom > 0 && rect.top < window.innerHeight;
+
+    if (isVisible) {
+      setSectionUrlHash(section.id);
+    }
+  }
 }
 
-const container = document.querySelector('#section-container');
-container.addEventListener('scroll', scrollHandler);
+function setSectionUrlHash(sectionId) {
+  history.replaceState(undefined, undefined, `#${sectionId}`);
+}
+
+function enableSnapScrolling() {
+  console.log(`ENABLEING SNAP SCROLLING`);
+  // body.style.setProperty('height', '100%');
+  // body.style.removeProperty('overflow-y');
+  // container.style.setProperty('height', '100%');
+  // setTimeout(() => {
+  //   container.style.setProperty('-webkit-overflow-scrolling', 'touch');
+  //   container.style.setProperty('overflow-anchor', 'none');
+  //   container.style.setProperty('overflow-y', 'auto');
+  //   container.style.setProperty('scroll-snap-type', 'y mandatory');
+  //   container.style.setProperty('scroll-snap-stop', 'normal');
+  //   container.style.setProperty('scroll-behavior', 'auto');
+  // }, 0);
+}
+
+function disableSnapScrolling() {
+  // body.style.setProperty('height', 'auto');
+  // body.style.setProperty('overflow-y', 'auto');
+  // container.style.setProperty('height', 'auto');
+  // setTimeout(() => {
+  //   container.style.removeProperty('-webkit-overflow-scrolling');
+  //   container.style.removeProperty('overflow-anchor');
+  //   container.style.removeProperty('overflow-y');
+  //   container.style.removeProperty('scroll-snap-type');
+  //   container.style.removeProperty('scroll-snap-stop');
+  //   container.style.removeProperty('scroll-behavior');
+  // }, 0);
+}
+
+function scrollToSection(sectionId) {
+  const section = document.querySelector(sectionId);
+
+  if (section) {
+    // disableSnapScrolling();
+    console.log(`Scrolling to ${sectionId} (${section.offsetTop}px)`);
+    setTimeout(() => window.scrollTo(0, section.offsetTop), 0);
+
+    // setTimeout(() => {
+    //   enableSnapScrolling();
+    // }, 2000);
+  }
+}
