64e1122f6abdbe5b5ef9f1d45eae2f5beca981d1
Filip Strandberg <filip.strandberg@hyph.com>
2024-05-08 16:41:58 +0200

    something with hover and glow

--- src/scripts/side-menu.js
+++ src/scripts/side-menu.js
@@ -1,25 +1,31 @@
 // Most of code from https://www.youtube.com/watch?v=bAwEj_mSzOs&ab_channel=Hyperplexed
 
 const TILE_SIZE_PX = 42;
+
 const section = document.getElementById('side-menu');
 const menu = section.getElementsByClassName('menu')[0];
 
 const createTile = () => {
   const tile = document.createElement('div');
   tile.classList.add('tile');
+  tile.innerHTML = 'test';
+
+  //   tile.addEventListener('mouseover', () => {
+  //     console.log('sdfdsf');
+  //     tile.style.setProperty('clip-path', 'none');
+  //   });
+  //   tile.addEventListener('mouseout', () => {
+  //     console.log('sdfdsf');
+  //     tile.style.removeProperty('clip-path');
+  //   });
 
   return tile;
 };
 
 const createTiles = (quantity) => {
-  Array.from(Array(quantity)).map((_, index) => {
+  return Array.from(Array(quantity)).map((_, index) => {
     menu.appendChild(createTile(index));
   });
-
-  const tile = document.createElement('div');
-  tile.classList.add('tile');
-
-  return tile;
 };
 
 const createGrid = () => {
