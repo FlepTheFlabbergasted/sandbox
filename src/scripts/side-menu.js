c62bb501855247832a555848bc3b43a1936778c5
Filip Strandberg <filip.strandberg@hyph.com>
2024-05-09 16:59:48 +0200

    create consts for every css class vars

--- src/scripts/side-menu.js
+++ src/scripts/side-menu.js
@@ -1,74 +1,96 @@
 // Tile code from https://www.youtube.com/watch?v=bAwEj_mSzOs&ab_channel=Hyperplexed
 
 const TILE_HEIGHT_PX = 42;
+const TILE_CSS_CLASS = 'tile';
+const TILE_ACTIVE_CSS_CLASS = 'active-tile';
+const TILE_ACTIVE_ABOVE_CSS_CLASS = 'active-tile-above';
+const TILE_ACTIVE_BELOW_CSS_CLASS = 'active-tile-below';
+const TILE_DATA_INDEX_ATTR = 'data-tile-index';
 
-const section = document.getElementById('side-menu');
-const menu = section.getElementsByClassName('menu')[0];
-const topIcons = ['fa-solid fa-music fa-lg', 'fa-solid fa-code fa-lg'];
-const bottomIcons = ['fa-regular fa-circle-user fa-lg', 'fa-solid fa-gear fa-lg'];
+const CSS_VAR_TILE_ROWS = '--side-menu-rows';
+const CURSOR_POINTER_CSS_CLASS = 'cursor-pointer';
+
+const TOP_ICONS = ['fa-solid fa-music fa-lg', 'fa-solid fa-code fa-lg'];
+const BOTTOM_ICONS = ['fa-regular fa-circle-user fa-lg', 'fa-solid fa-gear fa-lg'];
+
+const sectionEl = document.getElementById('side-menu');
+const menuEl = sectionEl.getElementsByClassName('menu')[0];
+
+const createIconHtml = (iconClasses) => {
+  return `<i class="${iconClasses}"></i>`;
+};
 
 const createTile = (index) => {
   const tile = document.createElement('div');
-  tile.classList.add('tile');
-  tile.setAttribute('data-tile-index', index);
+  tile.classList.add(TILE_CSS_CLASS);
+  tile.setAttribute(TILE_DATA_INDEX_ATTR, index);
 
   return tile;
 };
 
-const createTiles = () => {
-  menu.innerHTML = '';
-  let nrRows = Math.ceil(section.clientHeight / TILE_HEIGHT_PX);
-  let tiles = Array.from(Array(nrRows)).map((_, index) => createTile(index));
-  let bottomIconsCopy = [...bottomIcons];
+const addEventListenerToTile = (tile, index, nrRows) => {
+  tile.addEventListener('click', () => {
+    if (tile.classList.contains(TILE_ACTIVE_CSS_CLASS)) {
+      tile.classList.remove(TILE_ACTIVE_CSS_CLASS);
+      if (index < nrRows - 1) {
+        menuEl
+          .querySelector(`.${TILE_CSS_CLASS}[${TILE_DATA_INDEX_ATTR}='${index + 1}']`)
+          .classList.remove(TILE_ACTIVE_ABOVE_CSS_CLASS);
+      }
 
-  menu.style.setProperty('--side-menu-rows', nrRows);
+      if (index > 0) {
+        menuEl
+          .querySelector(`.${TILE_CSS_CLASS}[${TILE_DATA_INDEX_ATTR}='${index - 1}']`)
+          .classList.remove(TILE_ACTIVE_BELOW_CSS_CLASS);
+      }
+    } else {
+      menuEl
+        .querySelectorAll(`.${TILE_CSS_CLASS}`)
+        .forEach((tile) =>
+          tile.classList.remove(TILE_ACTIVE_CSS_CLASS, TILE_ACTIVE_ABOVE_CSS_CLASS, TILE_ACTIVE_BELOW_CSS_CLASS)
+        );
+
+      tile.classList.add(TILE_ACTIVE_CSS_CLASS);
+
+      if (index < nrRows - 1) {
+        menuEl
+          .querySelector(`.${TILE_CSS_CLASS}[${TILE_DATA_INDEX_ATTR}='${index + 1}']`)
+          .classList.add(TILE_ACTIVE_ABOVE_CSS_CLASS);
+      }
+
+      if (index > 0) {
+        menuEl
+          .querySelector(`.${TILE_CSS_CLASS}[${TILE_DATA_INDEX_ATTR}='${index - 1}']`)
+          .classList.add(TILE_ACTIVE_BELOW_CSS_CLASS);
+      }
+    }
+  });
+};
+
+const createTiles = () => {
+  let nrRows = Math.ceil(sectionEl.clientHeight / TILE_HEIGHT_PX);
+  let tiles = Array.from(Array(nrRows)).map((_, index) => createTile(index));
+  let bottomIconsCopy = [...BOTTOM_ICONS];
+
+  menuEl.innerHTML = '';
+  menuEl.style.setProperty(CSS_VAR_TILE_ROWS, nrRows);
 
   for (let i = 0; i < tiles.length; i++) {
-    if (topIcons[i]) {
-      tiles[i + 1].innerHTML = `<i class="${topIcons[i]}"></i>`;
-      tiles[i + 1].classList.add('cursor-pointer');
+    if (TOP_ICONS[i]) {
+      tiles[i + 1].innerHTML = createIconHtml(TOP_ICONS[i]);
+      tiles[i + 1].classList.add(CURSOR_POINTER_CSS_CLASS);
       addEventListenerToTile(tiles[i + 1], i + 1, nrRows);
     }
 
     if (i + 1 === tiles.length - bottomIconsCopy.length && bottomIconsCopy.length) {
-      tiles[i].innerHTML = `<i class="${bottomIconsCopy[0]}"></i>`;
-      tiles[i].classList.add('cursor-pointer');
+      tiles[i].innerHTML = createIconHtml(bottomIconsCopy[0]);
+      tiles[i].classList.add(CURSOR_POINTER_CSS_CLASS);
       bottomIconsCopy.shift();
       addEventListenerToTile(tiles[i], i, nrRows);
     }
 
-    menu.appendChild(tiles[i]);
+    menuEl.appendChild(tiles[i]);
   }
-};
-
-const addEventListenerToTile = (tile, index, nrRows) => {
-  tile.addEventListener('click', () => {
-    if (tile.classList.contains('active-tile')) {
-      tile.classList.remove('active-tile');
-
-      if (index > 0) {
-        menu.querySelector(`.tile[data-tile-index='${index - 1}']`).classList.remove('polygon-square-bot');
-      }
-
-      if (index < nrRows - 1) {
-        menu.querySelector(`.tile[data-tile-index='${index + 1}']`).classList.remove('polygon-square-top');
-      }
-    } else {
-      menu
-        .querySelectorAll(`.tile`)
-        .forEach((tile) => tile.classList.remove('active-tile', 'polygon-square-top', 'polygon-square-bot'));
-
-      tile.classList.add('active-tile');
-
-      if (index > 0) {
-        menu.querySelector(`.tile[data-tile-index='${index - 1}']`).classList.add('polygon-square-bot');
-      }
-
-      if (index < nrRows - 1) {
-        menu.querySelector(`.tile[data-tile-index='${index + 1}']`).classList.add('polygon-square-top');
-      }
-    }
-  });
 };
 
 window.addEventListener('load', () => createTiles());
