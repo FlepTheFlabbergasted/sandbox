0a5679e0948a687055f2fd61ba5b68d38521a587
Filip Strandberg <filip.strandberg@hyph.com>
2024-05-09 10:57:52 +0200

    divy out the icons

--- src/scripts/side-menu.js
+++ src/scripts/side-menu.js
@@ -4,6 +4,8 @@ const TILE_HEIGHT_PX = 42;
 
 const section = document.getElementById('side-menu');
 const menu = section.getElementsByClassName('menu')[0];
+const topIcons = ['fa-solid fa-music fa-lg', 'fa-solid fa-code fa-lg'];
+const bottomIcons = ['fa-regular fa-circle-user fa-lg', 'fa-solid fa-gear fa-lg'];
 
 const polygonSpeaker = 'polygon(100% 0%, 0% 0%, 0% 100%, 100% 100%, 100% 200%, 200% 200%, 200% -100%, 100% -100%)'; // "speaker" shape
 // const polygonSpeaker = 'polygon(0% 1%, 100% 1%, 100% 200%, 200% 200%, 200% -200%, 100% -200%, 100% 99%, 0% 99%)'; // "speaker" shape with -1% top n bot to prevent bleeding of box shadow
@@ -14,7 +16,7 @@ const createTile = (nrRows, index) => {
   const tile = document.createElement('div');
   tile.classList.add('tile');
   tile.setAttribute('data-tile-index', index);
-  tile.innerHTML = 'test';
+  //   tile.innerHTML = 'test';
 
   tile.addEventListener('mouseover', () => {
     if (index > 0) {
@@ -38,19 +40,27 @@ const createTile = (nrRows, index) => {
   return tile;
 };
 
-const createTiles = (nrRows) => {
-  return Array.from(Array(nrRows)).map((_, index) => {
-    menu.appendChild(createTile(nrRows, index));
-  });
-};
-
-const createGrid = () => {
+const createTiles = () => {
   menu.innerHTML = '';
-  let rows = Math.ceil(section.clientHeight / TILE_HEIGHT_PX);
-  menu.style.setProperty('--side-menu-rows', rows);
+  let nrRows = Math.ceil(section.clientHeight / TILE_HEIGHT_PX);
+  let tiles = Array.from(Array(nrRows)).map((_, index) => createTile(nrRows, index));
+  let bottomIconsCopy = [...bottomIcons];
 
-  createTiles(rows);
+  menu.style.setProperty('--side-menu-rows', nrRows);
+
+  for (let i = 0; i < tiles.length; i++) {
+    if (topIcons[i]) {
+      tiles[i + 1].innerHTML = `<i class="${topIcons[i]}"></i>`;
+    }
+
+    if (i + 1 === tiles.length - bottomIconsCopy.length) {
+      tiles[i].innerHTML = `<i class="${bottomIconsCopy[0]}"></i>`;
+      bottomIconsCopy.shift();
+    }
+
+    menu.appendChild(tiles[i]);
+  }
 };
 
-window.onload = () => createGrid();
-window.onresize = () => createGrid();
+document.body.onload = () => createTiles();
+window.onresize = () => createTiles();
