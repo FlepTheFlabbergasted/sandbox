7977f7762a6cdad9a2e4394b8c11b283bc191b2f
Filip Strandberg <filip.strandberg@hyph.com>
2024-12-28 17:00:40 +0100

    active reactor arrow

--- src/scripts/retrofuturism/reactors.js
+++ src/scripts/retrofuturism/reactors.js
@@ -3,6 +3,8 @@ import { getRandomInt } from '../../util/get-random-int';
 const sectionEl = document.getElementById('retrofuturism');
 const reactorsDiv = sectionEl.querySelector('#reactors');
 const reactorDivs = reactorsDiv.querySelectorAll('.reactor');
+const arrowRightDivs = reactorsDiv.querySelectorAll('.arrow-right');
+const arrowLeftDivs = reactorsDiv.querySelectorAll('.arrow-left');
 
 const TIMEOUT_MS = 2000;
 const ACTIVE_REACTORS_AT_A_TIME = 4;
@@ -12,8 +14,21 @@ const setRandomActive = () => {
     reactorDiv.classList.remove('active');
   }
 
+  for (const arrowRightDiv of arrowRightDivs) {
+    arrowRightDiv.classList.remove('active');
+  }
+
+  for (const arrowLeftDiv of arrowLeftDivs) {
+    arrowLeftDiv.classList.remove('active');
+  }
+
   for (let i = 0; i < ACTIVE_REACTORS_AT_A_TIME; i++) {
-    reactorDivs[getRandomInt(0, reactorDivs.length - 1)].classList.add('active');
+    const randomInt = getRandomInt(0, reactorDivs.length - 1);
+    const row = randomInt % 4;
+
+    reactorDivs[randomInt].classList.add('active');
+    arrowRightDivs[row].classList.add('active');
+    arrowLeftDivs[row].classList.add('active');
   }
 
   setTimeout(() => setRandomActive(), TIMEOUT_MS);
