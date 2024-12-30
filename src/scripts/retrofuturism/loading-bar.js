a2446f4fcb1f0528f737d3fcf2e4915f6d193106
Filip Strandberg <filip.strandberg@hyph.com>
2024-12-30 12:57:18 +0100

    loading bar

---
+++ src/scripts/retrofuturism/loading-bar.js
@@ -0,0 +1,21 @@
+import { getRandomInt } from '../../util/get-random-int';
+
+const sectionEl = document.getElementById('retrofuturism');
+const loadingBarDiv = sectionEl.querySelector('#loading-bar');
+
+const DELAY_MS = 500;
+const RESTART_DELAY_MS = 1000;
+
+const advanceLoader = (percentage) => {
+  loadingBarDiv.style.setProperty('--percentage-loaded', percentage);
+
+  if (percentage >= 100) {
+    setTimeout(() => advanceLoader(0), DELAY_MS + RESTART_DELAY_MS);
+  } else {
+    setTimeout(() => advanceLoader(percentage + getRandomInt(1, 10)), DELAY_MS);
+  }
+};
+
+window.addEventListener('load', () => {
+  advanceLoader(0);
+});
