2b284244c1e71830553e1c1913f95f4213c099c4
Filip Strandberg <filip.strandberg@hyph.com>
2025-01-03 16:34:01 +0100

    rotate and animate string rings

--- src/scripts/string-rings.js
+++ src/scripts/string-rings.js
@@ -1,31 +1,65 @@
 const sectionEl = document.getElementById('string-rings');
 
-const sliders = sectionEl.querySelectorAll(`input`);
+const borderRadiusSliders = sectionEl.querySelectorAll(`input[data-slider-type='border-radius']`);
 const sliderTopLeft = sectionEl.querySelectorAll(`input[name='top-left']`)[0];
 const sliderTopRight = sectionEl.querySelectorAll(`input[name='top-right']`)[0];
-const sliderBottomRight = sectionEl.querySelectorAll(`input[name='bottom-right']`)[0];
 const sliderBottomLeft = sectionEl.querySelectorAll(`input[name='bottom-left']`)[0];
-const sliderTopLeft2 = sectionEl.querySelectorAll(`input[name='top-left2']`)[0];
-const sliderTopRight2 = sectionEl.querySelectorAll(`input[name='top-right2']`)[0];
-const sliderBottomRight2 = sectionEl.querySelectorAll(`input[name='bottom-right2']`)[0];
-const sliderBottomLeft2 = sectionEl.querySelectorAll(`input[name='bottom-left2']`)[0];
+const sliderBottomRight = sectionEl.querySelectorAll(`input[name='bottom-right']`)[0];
 
-const ringEl = sectionEl.getElementsByClassName('ring')[0];
-const resultEl = sectionEl.querySelector('#results');
+const rotationSliders = sectionEl.querySelectorAll(`input[data-slider-type='rotation']`);
+const sliderRotationX = sectionEl.querySelectorAll(`input[name='rotation-x']`)[0];
+const sliderRotationY = sectionEl.querySelectorAll(`input[name='rotation-y']`)[0];
+const sliderRotationZ = sectionEl.querySelectorAll(`input[name='rotation-z']`)[0];
 
-const addEventListenersToSliders = () => {
-  Array.from(sliders).forEach((slider) =>
+const rings = sectionEl.getElementsByClassName('ring');
+const ringSelectionRadioButtons = sectionEl.querySelectorAll(`input[type='radio']`);
+
+const borderRadiusResultsEl = sectionEl.querySelector('#border-radius-results');
+const rotationResultsEl = sectionEl.querySelector('#rotation-results');
+
+let ringSelectionIndex = 0;
+
+const addEventListeners = () => {
+  Array.from(ringSelectionRadioButtons).forEach((radioButton) =>
+    radioButton.addEventListener('input', (event) => {
+      ringSelectionIndex = Number(event.target.value);
+
+      const computedStyle = getComputedStyle(rings[ringSelectionIndex]);
+
+      sliderTopLeft.value = computedStyle.getPropertyValue('--border-radius-top-left').replace('%', '');
+      sliderTopRight.value = computedStyle.getPropertyValue('--border-radius-top-right').replace('%', '');
+      sliderBottomRight.value = computedStyle.getPropertyValue('--border-radius-bottom-left').replace('%', '');
+      sliderBottomLeft.value = computedStyle.getPropertyValue('--border-radius-bottom-right').replace('%', '');
+
+      sliderRotationX.value = computedStyle.getPropertyValue('--rotation-x');
+      sliderRotationY.value = computedStyle.getPropertyValue('--rotation-y');
+      sliderRotationZ.value = computedStyle.getPropertyValue('--rotation-z');
+
+      borderRadiusResultsEl.innerHTML = `${sliderTopLeft.value}% ${sliderTopRight.value}% ${sliderBottomLeft.value}% ${sliderBottomRight.value}%`;
+      rotationResultsEl.innerHTML = `rotateX(${sliderRotationX.value}deg) rotateY(${sliderRotationY.value}deg) rotateZ(${sliderRotationZ.value}deg)`;
+    })
+  );
+
+  Array.from(borderRadiusSliders).forEach((slider) =>
     slider.addEventListener('input', () => {
-      const newBorderRadius = `
-      ${sliderTopLeft.value}%  ${sliderTopRight.value}%  ${sliderBottomRight.value}%  ${sliderBottomLeft.value}% /
-      ${sliderTopLeft2.value}% ${sliderTopRight2.value}% ${sliderBottomRight2.value}% ${sliderBottomLeft2.value}%`;
+      rings[ringSelectionIndex].style.setProperty('--border-radius-top-left', `${sliderTopLeft.value}%`);
+      rings[ringSelectionIndex].style.setProperty('--border-radius-top-right', `${sliderTopRight.value}%`);
+      rings[ringSelectionIndex].style.setProperty('--border-radius-bottom-left', `${sliderBottomLeft.value}%`);
+      rings[ringSelectionIndex].style.setProperty('--border-radius-bottom-right', `${sliderBottomRight.value}%`);
+      borderRadiusResultsEl.innerHTML = `${sliderTopLeft.value}%  ${sliderTopRight.value}%  ${sliderBottomRight.value}%  ${sliderBottomLeft.value}%`;
+    })
+  );
 
-      ringEl.style.borderRadius = newBorderRadius;
-      resultEl.innerHTML = newBorderRadius;
+  Array.from(rotationSliders).forEach((slider) =>
+    slider.addEventListener('input', () => {
+      rings[ringSelectionIndex].style.setProperty('--rotation-x', `${sliderRotationX.value}deg`);
+      rings[ringSelectionIndex].style.setProperty('--rotation-y', `${sliderRotationY.value}deg`);
+      rings[ringSelectionIndex].style.setProperty('--rotation-z', `${sliderRotationZ.value}deg`);
+      rotationResultsEl.innerHTML = `rotateX(${sliderRotationX.value}deg) rotateY(${sliderRotationY.value}deg) rotateZ(${sliderRotationZ.value}deg)`;
     })
   );
 };
 
 window.addEventListener('load', () => {
-  addEventListenersToSliders();
+  addEventListeners();
 });
